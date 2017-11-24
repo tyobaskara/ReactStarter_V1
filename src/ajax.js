62c0c209b6b24389a279f98b7413a61b5e56bc51
Prasetya Aji <prasetya.aji@reservasi.com>
2017-11-24 11:31:47 +0700

    ajax fetch

--- src/ajax.js
+++ src/ajax.js
@@ -4,6 +4,31 @@ import { Helmet } from 'react-helmet';
 import { Navigation } from './navigation';
 import {Title, SubTitle} from './home';
 
+const fetchComments = () => {
+    let url = 'https://jsonplaceholder.typicode.com/posts/1/comments';
+    let toAppend = document.getElementsByClassName('list-ajax')[0];
+
+    fetch(url).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => console.log(networkError.message)
+    ).then(jsonResponse => {
+        toAppend.innerHTML = '';
+        for(let i=0;i<jsonResponse.length;i++){
+            var markup = `
+                <div>name: ${jsonResponse[i].name}</div>
+                <div>email: ${jsonResponse[i].email}</div>
+                <div>comment: ${jsonResponse[i].body.replace(/\n/g, ' ')}</div>`;
+            var node = document.createElement('li');
+            node.innerHTML = markup;
+
+            toAppend.append(node);
+        }
+    });
+}
+
 class Ajax extends React.Component {
     render() {
 
@@ -19,10 +44,17 @@ class Ajax extends React.Component {
                 <div className="container-fluid wrapper-outside">
                     <div className="container">
                         <h1>This is Ajax..</h1>
+                        <ul className="list-ajax">Loading...</ul>
                     </div>
                 </div>
             </div>
         )
+    }
+
+    componentDidMount(){
+        setTimeout(function(){
+            fetchComments();
+        },500);
     }
 };
 
